74e362b16334b02264786864fd128d814e17b8f1
kmeing11-dotcom <kmeing11@gmail.com>
2026-07-27 01:31:55 +0400

    Delete run.py

--- run.py
+++
@@ -1,9 +0,0 @@
-import uvicorn
-import sys
-import os
-
-# Добавляем путь к папке clean_architecture_practice
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-
-if __name__ == "__main__":
-    uvicorn.run("clean_architecture_practice.app.main:app", host="0.0.0.0", port=10000)
