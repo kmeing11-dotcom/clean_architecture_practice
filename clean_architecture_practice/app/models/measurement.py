341f1f3cd8c2f46710cfa2ae759ea2d65d578428
kmeing11-dotcom <kmeing11@gmail.com>
2026-07-27 02:10:45 +0400

    Update measurement.py

--- clean_architecture_practice/app/models/measurement.py
+++ clean_architecture_practice/app/models/measurement.py
@@ -1,12 +1,13 @@
 from sqlalchemy import Column, Integer, Float, DateTime
 from app.core.database import Base
 from datetime import datetime
+from sqlalchemy import Column, Integer, Float, DateTime, BigInteger
 
 class Measurement(Base):
     __tablename__ = "measurements"
 
     id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(Integer, index=True)
+    user_id = Column(BigInteger, index=True)
     weight = Column(Float)
     height = Column(Float)
     imt = Column(Float)
