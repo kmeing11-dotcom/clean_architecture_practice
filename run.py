1ace19dbccb2e223ec632f1560593dcb1f4d9323
kmeing11-dotcom <kmeing11@gmail.com>
2026-07-27 01:37:34 +0400

    Update run.py

--- run.py
+++ run.py
@@ -2,7 +2,8 @@ import uvicorn
 import sys
 import os
 
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+# Добавляем путь к папке clean_architecture_practice в sys.path
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'clean_architecture_practice'))
 
 if __name__ == "__main__":
     uvicorn.run("app.main:app", host="0.0.0.0", port=10000)
